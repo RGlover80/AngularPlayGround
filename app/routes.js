2a54f521f03fe1bb0cd65c3abcf37907777dae6f
RGlover80 <richard.glover@nostringnetworks.com>
2014-05-19 11:00:44 +0100

    route for server

--- app/routes.js
+++ app/routes.js
@@ -40,8 +40,8 @@ module.exports = function(app) {
 
 	// frontend routes =========================================================
 	// route to handle all angular requests
-	//app.get('*', function(req, res) {
-	//	res.sendfile('./public/index.html'); // load our public/index.html file
-	//});
+	app.get('*', function(req, res) {
+		res.sendfile('./public/index.html'); // load our public/index.html file
+	});
 
 };
